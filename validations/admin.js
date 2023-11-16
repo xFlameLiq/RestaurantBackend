40b97a328ab07a71e0fb30edec2caf68d05abde9
xFlameLiq <benjamincore2002@hotmail.com>
2023-11-16 10:05:00 -0600

    Field phone fixed (min 10)

--- validations/admin.js
+++ validations/admin.js
@@ -4,7 +4,7 @@ exports.createAdminSchema = Joi.object({
     name: Joi.string().pattern(/^[A-Za-z\s'-]+$/).max(100).required(),
     lastname: Joi.string().pattern(/^[A-Za-z\s'-]+$/).max(100).required(),
     address: Joi.string().pattern(/^[A-Za-z0-9\s#'-]+$/).max(150).required(),
-    phone: Joi.string().pattern(/^\d+$/).max(10).required(),
+    phone: Joi.string().pattern(/^\d+$/).min(10).max(10).required(),
     username: Joi.string().pattern(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/).max(100).required(),
     password: Joi.string().min(8).max(50).required()
 });
